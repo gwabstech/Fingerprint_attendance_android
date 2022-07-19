59869505b9960b9009eef51cedd47829a56e44df
Abubakar Abdullahi Gwabare <gwabstech@gmail.com>
2022-07-20 00:46:49 +0100

    Initial commit

---
+++ app/src/main/java/com/gwabs/myproject/MyApplication.java
@@ -0,0 +1,6 @@
+package com.gwabs.myproject;
+import android.app.Application;
+public class MyApplication extends Application {
+    Boolean globalVariable=false;
+}
+
---
+++ app/src/main/java/com/gwabs/myproject/RegistrationadminActivity.java
@@ -0,0 +1,51 @@
+package com.gwabs.myproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegistrationadminActivity extends AppCompatActivity {
+
+
+    private EditText Email;
+    private EditText Password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrationadmin);
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+
+        Email = (EditText)findViewById(R.id.etName1);
+        Password = (EditText)findViewById(R.id.etPassword1);
+
+    }
+    public void OnAllowClick(View view) {
+        String userName=Email.getText().toString();
+        String userPassword=Password.getText().toString();
+        if (Email.getText().toString().equals("")) {
+            Email.setError("Enter a valid Email Id");
+            Email.requestFocus();
+            return;
+        }
+        if (Password.getText().toString().equals("")) {
+            Password.setError("Enter Valid Admin Password");
+            Password.requestFocus();
+            return;
+        }
+        if((userName.equals("Admin@admin.com")) && (userPassword.equals("123456"))){
+            Intent intent = new Intent(RegistrationadminActivity.this, RegistrationActivity.class);
+            startActivity(intent);
+            finish();
+        } else {
+            Toast.makeText(RegistrationadminActivity.this, "Wrong Credentials!", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+}
